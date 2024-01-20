256d25cbe5bea7fbb279b781348cb7429d83371a
ygursu <yasemingrs@gmail.com>
2024-01-20 20:20:35 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int tahmin, hak = 5;
+bool durum = false;
+Random rastgele = new Random();
+int rastgeleSayi = rastgele.Next(0, 10);
+for (int kalanHak = hak; kalanHak > 0; kalanHak--)
+{
+    Console.WriteLine("Hak {0}", kalanHak);
+    Console.Write("Tahmininiz :");
+    tahmin = int.Parse(Console.ReadLine());
+    if (tahmin == rastgeleSayi)
+    {
+        Console.WriteLine("Tebrikler");
+        durum = true;
+        break;
+    }
+    else if (rastgeleSayi > tahmin && kalanHak != 1)
+        Console.WriteLine("Daha büyük bir sayı girin");
+    else if (rastgeleSayi < tahmin && kalanHak != 1)
+        Console.WriteLine("Daha küçük bir sayı girin");
+}
+if (!durum)
+{
+    Console.WriteLine("Tekrar Deneyin!");
+    Console.WriteLine("Sayı : {0}", rastgeleSayi);
+}
+Console.
